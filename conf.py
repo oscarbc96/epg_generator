96c1cf9c718aad44031952138c2f5a7bb6950b85
Oscar Blanco <oscarbc@me.com>
2019-01-05 19:12:27 +0100

    Move variables to a conf file

---
+++ conf.py
@@ -0,0 +1,10 @@
+OUTPUT_FOLDER = "output/"
+EPG_FILE = OUTPUT_FOLDER + "epg.xml"
+
+MOVISTAR_AJAX_URL = "http://comunicacion.movistarplus.es/wp-admin/admin-ajax.php"
+MOVISTAR_CHANNEL_LOGO_URL = "http://www.movistarplus.es/recorte/m-NEO/canal/"
+MOVISTAR_DESCRIPTION_URL = "http://comunicacion.movistarplus.es/detalle-de-programacion/?cee="
+
+DOWNLOAD_EXTRA_INFO = True
+
+DAYS_TO_DOWNLOAD = 3
