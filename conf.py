34a6d653158b3a014b60c6557c2200fe728d3126
Oscar Blanco <oscarbc@me.com>
2019-01-05 21:04:24 +0100

    Disable get extra info

--- conf.py
+++ conf.py
@@ -5,6 +5,6 @@ MOVISTAR_AJAX_URL = "http://comunicacion.movistarplus.es/wp-admin/admin-ajax.php
 MOVISTAR_CHANNEL_LOGO_URL = "http://www.movistarplus.es/recorte/m-NEO/canal/"
 MOVISTAR_DESCRIPTION_URL = "http://comunicacion.movistarplus.es/detalle-de-programacion/?cee="
 
-DOWNLOAD_EXTRA_INFO = True
+DOWNLOAD_EXTRA_INFO = False
 
 DAYS_TO_DOWNLOAD = 3
