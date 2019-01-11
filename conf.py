6617894f444abc4cce4c80f1b9e820219d0dabde
Oscar Blanco <oscarbc@me.com>
2019-01-11 19:08:58 +0100

    Update conf for paths

--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@ SCRIPT_FOLDER = dirname(realpath(__file__))
 
 CACHE_FOLDER = join(SCRIPT_FOLDER, "cache/")
 OUTPUT_FOLDER = join(SCRIPT_FOLDER, "output/")
-EPG_FILE = OUTPUT_FOLDER + "epg.xml"
+EPG_FILE = join(OUTPUT_FOLDER, "epg.xml")
 
 MOVISTAR_AJAX_URL = "http://comunicacion.movistarplus.es/wp-admin/admin-ajax.php"
 MOVISTAR_CHANNEL_LOGO_URL = "http://www.movistarplus.es/recorte/m-NEO/canal/"
